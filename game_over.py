0117f224007eefb6202fabb48ce32c8816b1bfa3
Murilo Lucena <135135416+ilmoruuu@users.noreply.github.com>
2023-09-10 17:32:29 -0300

    game_over.py

--- game_over.py
+++ game_over.py
@@ -18,7 +18,7 @@ pygame.display.set_caption('MENU') #legenda da tela
 #som
 click = pygame.mixer.Sound('sons/click.mp3')
 pygame.mixer.init()
-pygame.mixer.music.load('sons/intro.mp3')
+pygame.mixer.music.load('sons/death.mp3')
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1)
 
